7a8fed5056bb10f022db24a5138f12065186177f
arcobaleno <jymark.borja@gmail.com>
2025-02-21 15:41:15 +0800

    add: template for logged in users, routes and twig render function

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -12,8 +12,24 @@ final class DashboardController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
-        ]);
+        return $this->render('dashboard/pages/dashboard.html.twig');
+    }
+
+    #[Route('/pages', name: 'pages')]
+    public function pages(): Response
+    {
+        return $this->render('dashboard/pages/pages.html.twig');
+    }
+
+    #[Route('/account', name: 'account')]
+    public function account(): Response
+    {
+        return $this->render('dashboard/pages/account.html.twig');
+    }
+
+    #[Route('/inbox', name: 'inbox')]
+    public function inbox(): Response
+    {
+        return $this->render('dashboard/pages/inbox.html.twig');
     }
 }
