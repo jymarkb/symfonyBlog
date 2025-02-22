761ea56e7224950955b72634516de4cce4236487
arcobaleno <jymark.borja@gmail.com>
2025-02-22 11:58:19 +0800

    update: init files for home and dashboard

--- app/Resources/js/home/home.tsx
+++ app/Resources/js/home/home.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FeaturedNavigation } from './featured/navigation';
 import { CarouselDemo } from './CarouselSize';
+import { initSideBarNavigation } from '../dashboard/components/SidebarNavigation';
 
 class Home {
   constructor() {
-    new FeaturedNavigation(this);
+    this.init();
+  }
 
+  init() {
     const containerDiv = document.querySelector('#carousel');
     if (containerDiv) {
       ReactDOM.createRoot(containerDiv).render(<CarouselDemo />);
     }
 
     const headerProfile = document.querySelector('.randomBg');
-
     if (headerProfile instanceof HTMLElement) {
-      headerProfile.style.backgroundColor = this.randomBg();
+      headerProfile.style.backgroundColor = this.initRandomProfileBG();
     }
+
+    initSideBarNavigation();
   }
 
-  randomBg = () => {
+  initRandomProfileBG() {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  };
+  }
 }
 
 new Home();
