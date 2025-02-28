6003639cc6c8fc1ffac85ae19bee54e801765f8c
arcobaleno <jymark.borja@gmail.com>
2025-02-28 20:26:00 +0800

    add: logic for get latest blog for homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\BlogRepository;
 
 #[Route('/', name: 'home.')]
 final class HomeController extends AbstractController
 {
+    private BlogRepository $blogRepository;
+    public function __construct(BlogRepository $blogRepository){
+        $this->blogRepository = $blogRepository;
+    }
 
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $data = $this->blogRepository->getLatestBlogPost();
+        return $this->render('home/index.html.twig', [
+            'data' => $data
+        ]);
     }
 }
