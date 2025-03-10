8ad992238642538fd5c33b4937e871258a611107
arcobaleno <jymark.borja@gmail.com>
2025-03-10 14:51:40 +0800

    refactor: extract action from thumbnail component feat: add support for edit page

--- app/Resources/js/dashboard/components/ThumbnailUploader.ts
+++ app/Resources/js/dashboard/components/ThumbnailUploader.ts
@@ -1,14 +1,16 @@
-import { toast } from 'sonner';
+import {
+  handleFileValidation,
+  showImage,
+  removeImage,
+} from './action/FileuploadAction';
 
-const showToastError = (message: string = 'Invalid File') => {
-  toast.error(message, {
-    description: 'Please upload a valid image file (JPG, JPEG, PNG, or WEBP).',
-    duration:1500
-  });
-};
-
-const fileTypes = ['image/jpg', 'image/jpeg', 'image/png', 'image/webp'];
-const ThumbnailUploader = () => {
+const ThumbnailUploader = ({
+  isEditPage,
+  targetField,
+}: {
+  isEditPage: boolean;
+  targetField: string;
+}) => {
   const fileInput = document.getElementById(
     'create_new_page_htmlThumbnail',
   ) as HTMLInputElement | null;
@@ -34,6 +36,20 @@ const ThumbnailUploader = () => {
   )
     return;
 
+  if (isEditPage) {
+    // preload the image from storage
+    const targetInput = document.getElementById(
+      targetField,
+    ) as HTMLInputElement;
+    const imgPath = targetInput?.getAttribute('data-src');
+
+    if (imgPath) {
+      imgViewer.src = imgPath;
+      imgContainer.classList.remove('hidden');
+      fileUploadLabel.classList.add('hidden');
+    }
+  }
+
   const handleDrag = (e: DragEvent, add: boolean) => {
     e.preventDefault();
     e.stopPropagation();
@@ -41,53 +57,34 @@ const ThumbnailUploader = () => {
     fileUploadLabel.classList.toggle('border-dashed', !add);
   };
 
-  const handleFileValidation = (file: File | null): file is File => {
-    if (!file || !fileTypes.includes(file.type)) {
-      showToastError();
-      return false;
-    }
-    return true;
-  };
-
   const handleDrop = (e: DragEvent) => {
     e.preventDefault();
     e.stopPropagation();
 
     const file = e?.dataTransfer?.files[0] ?? null;
-    if (!handleFileValidation(file)) return;
+    if (!file || !handleFileValidation(file)) return;
 
     const dataTransfer = new DataTransfer();
     dataTransfer.items.add(file);
     fileInput.files = dataTransfer.files;
 
-    showImage(file);
+    showImage(file, imgViewer, imgContainer, fileUploadLabel);
   };
 
   const handleFileChange = (e: Event) => {
     const target = e.target as HTMLInputElement;
     const file = target.files?.[0] ?? null;
-    if (!handleFileValidation(file)) {
+    if (!file || !handleFileValidation(file)) {
       target.value = '';
       return;
     }
-    showImage(file);
+    showImage(file, imgViewer, imgContainer, fileUploadLabel);
   };
 
-  const showImage = (file: File) => {
-    if (!imgViewer || !imgContainer || !fileUploadLabel) return;
-    imgViewer.src = URL.createObjectURL(file);
-    imgContainer.classList.remove('hidden');
-    fileUploadLabel.classList.add('hidden');
-  };
-
-  const removeImage = (e: MouseEvent) => {
+  const handleRemoveImage = (e: MouseEvent) => {
     e.stopPropagation();
     e.preventDefault();
-    if (!imgViewer || !imgContainer || !fileUploadLabel || !fileInput) return;
-    imgViewer.src = '';
-    imgContainer.classList.add('hidden');
-    fileUploadLabel.classList.remove('hidden');
-    fileInput.value = '';
+    removeImage(imgViewer, imgContainer, fileUploadLabel, fileInput);
   };
 
   fileUploadLabel.addEventListener('dragenter', (e) => handleDrag(e, true));
@@ -95,7 +92,7 @@ const ThumbnailUploader = () => {
   fileUploadLabel.addEventListener('dragover', (e) => e.preventDefault());
   fileUploadLabel.addEventListener('drop', handleDrop);
   fileInput.addEventListener('change', handleFileChange);
-  removeUpload.addEventListener('click', removeImage);
+  removeUpload.addEventListener('click', handleRemoveImage);
 };
 
 export default ThumbnailUploader;
