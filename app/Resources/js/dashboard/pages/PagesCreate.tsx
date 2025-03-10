f04bdee5f884d9a08bd49d1bf212310c997115ce
arcobaleno <jymark.borja@gmail.com>
2025-03-10 14:48:33 +0800

    update: pages script init files script and css; compilation and import

--- app/Resources/js/dashboard/pages/PagesCreate.tsx
+++ app/Resources/js/dashboard/pages/PagesCreate.tsx
@@ -1,13 +1,11 @@
-import { blogContentConfig } from '../components/config/blogContentConfig';
-import { cssContentConfig } from '../components/config/cssContentConfig';
-import { jsContentConfig } from '../components/config/jsContentConfig';
 import { initSideBarNavigation } from '../components/SidebarNavigation';
 import {
   htmlEditorConfig,
   cssEditorConfig,
   jsEditorConfig,
+  fileUploadConfig,
 } from '../components/config/pagesEditorConfig';
-import { initEditor } from '../components/Editor';
+import { Editor } from '../components/Editor';
 import ThumbnailUploader from '../components/ThumbnailUploader';
 import ScrollListener from '../components/ScrollListener';
 import ToastMessage from '../components/ToastMessage';
@@ -22,29 +20,26 @@ class PagesCreate {
   init() {
     initSideBarNavigation();
     this.initTinyFormEditor(); // for html,css,js editor
-    ThumbnailUploader();
+    ThumbnailUploader(fileUploadConfig());
     ScrollListener(); // for header action btn
     ToastMessage();
     popUpModal('btnPreview', () => PreviewPopUpData());
   }
 
-  async initTinyFormEditor() {
+  initTinyFormEditor() {
     const editorConfigs = [
       {
         ...htmlEditorConfig(),
-        contentConfig: blogContentConfig('create_new_page_htmlContent'),
       },
       {
         ...cssEditorConfig(),
-        contentConfig: cssContentConfig('create_new_page_htmlStyle'),
       },
       {
         ...jsEditorConfig(),
-        contentConfig: jsContentConfig('create_new_page_htmlScript'),
       },
     ];
 
-    editorConfigs.forEach(initEditor);
+    editorConfigs.forEach(Editor);
   }
 }
 new PagesCreate();
