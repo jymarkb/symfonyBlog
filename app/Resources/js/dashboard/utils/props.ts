1075f12174d3749c3a2c31f761275457ae137b48
arcobaleno <jymark.borja@gmail.com>
2025-03-15 10:46:27 +0800

    remove: unused popup wrapper

--- app/Resources/js/dashboard/utils/props.ts
+++ app/Resources/js/dashboard/utils/props.ts
@@ -1,15 +1,5 @@
 import React from 'react';
 
-export type PopupProps = {
-  containerEl?: HTMLElement;
-  onReady?: (props: PopupWrapperProps) => void;
-};
-
-export type PopupWrapperProps = {
-  popup: HTMLElement | null;
-  popupContainer: HTMLElement | null;
-};
-
 export type TableDropDownProps = {
   targetX: number;
   targetY: number;
@@ -38,3 +28,6 @@ export type PageFormData = {
   htmlStyle?: string;
   htmlScript?: string;
 };
+
+export type FiltersType = Record<string, Record<string, number> | string>;
+
