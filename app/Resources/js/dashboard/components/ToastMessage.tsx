628c8a0b83e5e428a6c3c359c911ed18b65e5c6d
arcobaleno <jymark.borja@gmail.com>
2025-03-16 13:34:35 +0800

    update: toast promp template (title & message)

--- app/Resources/js/dashboard/components/ToastMessage.tsx
+++ app/Resources/js/dashboard/components/ToastMessage.tsx
@@ -6,16 +6,39 @@ const ToastMessage = () => {
   const el = document.getElementById('toaster') as HTMLElement;
   ReactDOM.createRoot(el).render(<Toaster richColors />);
 
+  const colorClasses: Record<
+    'success' | 'error' | 'info' | 'warning' | 'loading',
+    string
+  > = {
+    success: 'text-green-900',
+    error: 'text-red-900',
+    info: 'text-blue-900',
+    warning: 'text-yellow-900',
+    loading: 'text-gray-900',
+  };
+
   // Listen for custom toast events
   document.addEventListener('show-toast', (event: Event) => {
-    const customEvent = event as CustomEvent<{ type: string; message: string }>;
-    const { type, message } = customEvent.detail;
+    const customEvent = event as CustomEvent<{
+      type: string;
+      title: string;
+      message: string;
+    }>;
+    const { type, title, message } = customEvent.detail;
 
-    if (type === 'success') {
-      toast.success(message, { duration: 1500 });
-    } else if (type === 'error') {
-      toast.error(message, { duration: 1500 });
-    }
+    toast[type as 'success' | 'error' | 'info' | 'warning' | 'loading'](
+      title as string,
+      {
+        description: (
+          <p
+            className={`text-sm font-semibold ${colorClasses[type as keyof typeof colorClasses]}`}
+          >
+            {message}
+          </p>
+        ),
+        duration: 1500,
+      },
+    );
   });
 };
 
