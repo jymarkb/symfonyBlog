1e7de2130a9ecb97a5ebe968b4384949651b79e9
arcobaleno <jymark.borja@gmail.com>
2025-03-19 11:54:56 +0800

    add: sidebar cookie & profilebg  storage

--- app/Resources/js/dashboard/components/SidebarNavigation.tsx
+++ app/Resources/js/dashboard/components/SidebarNavigation.tsx
@@ -15,20 +15,31 @@ export const initSideBarNavigation = (
   const sidebarWrapper = document.getElementById(wrapperId);
   // const mainContentWrapper = document.getElementById(mainContent);
   const btnSidebar = document.getElementById(buttonId);
+  const isOpen = document.cookie;
 
-  if (btnSidebar) {
-    btnSidebar.addEventListener('click', () => {
-      if (window.innerWidth < 1024) {
-        btnSidebar?.classList.toggle('active');
-        sidebarWrapper?.classList.toggle('w-72');
-        sidebarWrapper?.classList.toggle('w-0');
-        // mainContentWrapper?.classList.toggle('ml-72');
-      } else {
-        btnSidebar?.classList.toggle('active');
-        sidebarWrapper?.classList.toggle('w-72');
-        sidebarWrapper?.classList.toggle('w-0');
-        // mainContentWrapper?.classList.toggle('ml-72');
-      }
-    });
-  }
+  if (!btnSidebar) return;
+
+  const SideBarAction = () => {
+    if (window.innerWidth < 1024) {
+      btnSidebar?.classList.toggle('active');
+      sidebarWrapper?.classList.toggle('w-72');
+      sidebarWrapper?.classList.toggle('w-0');
+    } else {
+      btnSidebar?.classList.toggle('active');
+      sidebarWrapper?.classList.toggle('w-72');
+      sidebarWrapper?.classList.toggle('w-0');
+    }
+  };
+
+  btnSidebar.addEventListener('click', () => {
+    console.log(isOpen);
+
+    if (!isOpen) {
+      document.cookie = 'sidebarCookie=1; max-age=31536000;';
+    } else {
+      document.cookie = 'sidebarCookie=; max-age=0;';
+    }
+
+    SideBarAction();
+  });
 };
