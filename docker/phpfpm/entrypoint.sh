cb0bd5c058bf0ab9d5c8d69a0c62875c9c9461ca
arcobaleno <jymark.borja@gmail.com>
2025-03-31 21:56:45 +0800

    fix: entrypoint cache permission

--- docker/phpfpm/entrypoint.sh
+++ docker/phpfpm/entrypoint.sh
@@ -1,23 +1,24 @@
+#!/bin/sh
+set -e
+
+# Ensure /www/var exists
+mkdir -p /www/var
+
+# Set correct ownership and permissions for /www/var
+chown -R www-data:www-data /www/var
+chmod -R 777 /www/var
+
+echo "Permissions for /www/var fixed."
+
+# Warmup cache
 php bin/console cache:warmup
 retCacheWarmer=$?
-if [ $retCacheWarmer -eq 1 ]; then
-  echo "cannot get cache warmer, exiting"
+
+if [ $retCacheWarmer -ne 0 ]; then
+  echo "Cannot get cache warmer, exiting"
   exit $retCacheWarmer
 fi
 
-chmod -R 777 /www/var
+echo "Entrypoint success"
 
-if [[ -z "$RUN_COMMAND" ]]; then
-  echo "Starting $@...."
-  exec "$@"
-else
-  echo "Running console command...."
-  echo "\n"
-  printenv
-  echo "\n"
-  php /www/bin/console $RUN_COMMAND
-  CODE=$?
-  exit ${CODE}
-fi
-
-echo "entrypoint success"
+exec "$@"
