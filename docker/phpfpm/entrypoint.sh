f66e84d6c18a34daad64221f3f4b846c81c8188d
arcobaleno <jymark.borja@gmail.com>
2025-03-31 21:44:41 +0800

    update: entrypoint  command

--- docker/phpfpm/entrypoint.sh
+++ docker/phpfpm/entrypoint.sh
@@ -5,4 +5,19 @@ if [ $retCacheWarmer -eq 1 ]; then
   exit $retCacheWarmer
 fi
 
+chmod -R 777 /www/var
+
+if [[ -z "$RUN_COMMAND" ]]; then
+  echo "Starting $@...."
+  exec "$@"
+else
+  echo "Running console command...."
+  echo "\n"
+  printenv
+  echo "\n"
+  php /www/bin/console $RUN_COMMAND
+  CODE=$?
+  exit ${CODE}
+fi
+
 echo "entrypoint success"
