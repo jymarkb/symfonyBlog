e7efc8c7a01bd3660f203a77f234dceb9b909d42
arcobaleno <jymark.borja@gmail.com>
2025-04-01 20:09:29 +0800

    test: prod cache

--- docker/phpfpm/entrypoint.sh
+++ docker/phpfpm/entrypoint.sh
@@ -5,13 +5,14 @@ set -e
 mkdir -p /www/var
 
 # Set correct ownership and permissions for /www/var
-chown -R www-data:www-data /www/var
+# chown -R www-data:www-data /www/var
 chmod -R 777 /www/var
 
 echo "Permissions for /www/var fixed."
 
 # Warmup cache
-php bin/console cache:warmup
+php bin/console cache:clear --env=prod --no-warmup
+php bin/console cache:warmup --env=prod
 
 echo "Entrypoint success"
 
