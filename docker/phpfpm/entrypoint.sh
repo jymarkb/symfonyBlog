d07955b9891526b24f147cbb31ed09f06580cae2
arcobaleno <jymark.borja@gmail.com>
2025-04-01 20:31:58 +0800

    add: debug line entrypoint

--- docker/phpfpm/entrypoint.sh
+++ docker/phpfpm/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
-set -e
+set -ex
 
-# Ensure /www/var exists
+echo "Warming up cache"
 mkdir -p /www/var
-
-# Set correct ownership and permissions for /www/var
-# chown -R www-data:www-data /www/var
 chmod -R 777 /www/var
-
-echo "Permissions for /www/var fixed."
 
 # Warmup cache
 php bin/console cache:clear --env=prod --no-warmup
