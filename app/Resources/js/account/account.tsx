65e2ccd5224542c7fbe517aad9b810b7e89d4955
arcobaleno <jymark.borja@gmail.com>
2025-04-04 02:45:34 +0800

    update: account component (login, signup)

--- app/Resources/js/account/account.tsx
+++ app/Resources/js/account/account.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
 import { Popup } from '../dashboard/components/Popup';
 import { MobileHeader } from '../home/component/MobileHeader';
+import ShowPassword from '../component/ShowPassword';
+import ToastMessage from '../dashboard/components/ToastMessage';
+import { toast } from 'sonner';
 
 class Account {
-    constructor (){
-        this.init();
-    }
+  constructor() {
+    this.init();
+  }
+  init() {
+    ShowPassword('btnShowPass', '.inputPassword');
+    ToastMessage();
+    Popup({
+      btnTrigger: 'headerBtn',
+      isFilter: false,
+      popUpdata: <MobileHeader />,
+    });
+    this.initGoogle();
+  }
 
-    init(){
-        Popup({
-            btnTrigger: 'headerBtn',
-            isFilter: false,
-            popUpdata: <MobileHeader />,
-          });
-    }
+  initGoogle() {
+    const btnGoogle = document.querySelector('.btn-google');
 
+    btnGoogle?.addEventListener('click', () => {
+      toast.error('Not Working:', {
+        description: (
+          <p className="text-red-900">
+            <strong>Sign in</strong> or <strong>Sign up</strong> with Google
+            does not work at the moment.
+          </p>
+        ),
+        duration: 1500,
+      });
+    });
+  }
 }
 
-new Account;
+new Account();
