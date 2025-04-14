bd47c8603018190866f3079010667f08a1762c1b
arcobaleno <jymark.borja@gmail.com>
2025-04-14 22:59:56 +0800

    add: contact ts template

---
+++ app/Resources/js/contact/contact.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Popup } from '../dashboard/components/Popup';
+import { MobileHeader } from '../home/component/MobileHeader';
+import ToastMessage from '../dashboard/components/ToastMessage';
+import { toast } from 'sonner';
+class Contact {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    Popup({
+      btnTrigger: 'headerBtn',
+      isFilter: false,
+      popUpdata: <MobileHeader />,
+    });
+    ToastMessage();
+
+    this.initSubmitForm();
+    this.initFAQbutton();
+  }
+
+  initSubmitForm() {
+    const form = document.getElementById('google-form') as HTMLFormElement;
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const formData = new FormData(form);
+
+      try {
+        fetch(form.action, {
+          method: 'POST',
+          mode: 'no-cors',
+          body: formData,
+        });
+
+        toast.success('Success: Form Submit', {
+          description: (
+            <p className="text-sm font-semibold text-green-900">
+              Thank you! We&apos;ll be in touch soon.
+            </p>
+          ),
+        });
+
+        form.reset();
+      } catch (error) {
+        toast.error('Error: Form Submit', {
+          description: (
+            <div>
+              <p className="text-red-900">Cannot submit form at the moment.</p>
+              <p>{`${error}`}</p>
+            </div>
+          ),
+        });
+      }
+    });
+  }
+
+  initFAQbutton() {
+    const faqWrapper = document.getElementById(
+      'faq-row-wrapper',
+    ) as HTMLElement;
+
+    faqWrapper.addEventListener('click', (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      const btnTarget = target.closest('button');
+
+      if (!btnTarget) {
+        return;
+      }
+
+      const targetP = btnTarget.parentElement?.nextElementSibling as HTMLElement;
+
+      if (!targetP) {
+        return;
+      }
+
+      targetP.classList.toggle('show');
+    });
+  }
+}
+
+new Contact();
