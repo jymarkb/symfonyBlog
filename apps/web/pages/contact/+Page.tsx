749b6c83b162934504245d23c502f47d1dd82be9
arcobaleno <jymark.borja@gmail.com>
2026-05-01 21:12:56 +0800

    init - add tailwind and lucide

--- apps/web/pages/contact/+Page.tsx
+++ apps/web/pages/contact/+Page.tsx
@@ -1,3 +1,9 @@
+import { AppShell } from '@/layouts/AppShell'
+
 export default function Page() {
-  return <h1>Contact page</h1>
+  return (
+    <AppShell>
+      <h1>Contact page</h1>
+    </AppShell>
+  )
 }
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,11 @@
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import vike from 'vike/plugin'
 
 export default defineConfig({
-  plugins: [react(), vike()],
+  plugins: [react(), tailwindcss(), vike()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
