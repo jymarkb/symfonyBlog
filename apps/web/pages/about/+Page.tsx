28d9b08a06fdf0076574bc7455b36ff9f51eadce
arcobaleno <jymark.borja@gmail.com>
2026-05-03 14:32:01 +0800

    update - base header and pages

--- apps/web/pages/about/+Page.tsx
+++ apps/web/pages/about/+Page.tsx
@@ -1,11 +1,11 @@
 import { AppShell } from '@/layouts/AppShell'
-import { SiteHeader } from '@/components/common/SiteHeader'
+import { Header } from '@/components/common/Header'
 
 export default function Page() {
   return (
     <AppShell>
       <main>
-        <SiteHeader />
+        <Header />
         <h1>About page</h1>
       </main>
     </AppShell>
