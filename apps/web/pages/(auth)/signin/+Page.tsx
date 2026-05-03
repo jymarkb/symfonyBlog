27909178b29f1c478ada4b9896ca895dad9a8bfc
arcobaleno <jymark.borja@gmail.com>
2026-05-03 17:11:34 +0800

    init - setup for signin page

---
+++ apps/web/pages/(auth)/signin/+Page.tsx
@@ -0,0 +1,12 @@
+import { SignInIntro } from '@/features/auth/components/SignInIntro'
+import { SignInSidePanel } from '@/features/auth/components/SignInSidePanel'
+import { AuthShell } from '@/layouts/AuthShell'
+
+export default function Page() {
+  return (
+    <AuthShell side={<SignInSidePanel />}>
+      <SignInIntro />
+      {/* form renders here */}
+    </AuthShell>
+  )
+}
