e981bb97faf71291139e8531379bf310f78c4a0c
arcobaleno <jymark.borja@gmail.com>
2026-05-06 17:12:03 +0800

    fix - correct group layout

--- apps/web/pages/(admin)/dashboard/posts/+Page.tsx
+++ apps/web/pages/(admin)/dashboard/posts/+Page.tsx
@@ -1,12 +1,3 @@
-import { RequireAdmin } from "@/features/auth/guards";
-import { DashboardShell } from "@/layouts/DashboardShell";
-
 export default function Page() {
-  return (
-    <RequireAdmin>
-      <DashboardShell>
-        <h1>Dashboard Post page</h1>
-      </DashboardShell>
-    </RequireAdmin>
-  );
+  return <h1>Dashboard Post page</h1>;
 }
