6c7e4df356d135ae85d8bdc3b9ca870b42f3ac31
arcobaleno <jymark.borja@gmail.com>
2026-05-06 16:31:50 +0800

    remove - old or incorrect pages

--- apps/web/pages/(admin)/dashboard/pages.tsx
+++
@@ -1,7 +0,0 @@
-export default function dashboard() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
-}
--- apps/web/pages/(user)/profile/pages.tsx
+++
@@ -1,7 +0,0 @@
-export default function dashboard() {
-    return (
-        <div>
-            <h1>Profile</h1>
-        </div>
-    )
-}
