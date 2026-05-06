a52e477719095a721f2c42b3485b6e1874fff5aa
arcobaleno <jymark.borja@gmail.com>
2026-05-06 17:27:35 +0800

    fix - page visible issue on logged user

--- apps/web/pages/(auth)/signup/+Page.tsx
+++ apps/web/pages/(auth)/signup/+Page.tsx
@@ -1,11 +1,14 @@
 import { SignUpForm } from '@/features/auth/components/SignUpForm'
 import { SignUpSidePanel } from '@/features/auth/components/SignUpSidePanel'
+import { RequireGuest } from '@/features/auth/guards'
 import { AuthShell } from '@/layouts/AuthShell'
 
 export default function Page() {
   return (
-    <AuthShell side={<SignUpSidePanel />} sidePlacement="start">
-      <SignUpForm />
-    </AuthShell>
+    <RequireGuest>
+      <AuthShell side={<SignUpSidePanel />} sidePlacement="start">
+        <SignUpForm />
+      </AuthShell>
+    </RequireGuest>
   )
 }
