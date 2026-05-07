08aaccf587f4c2f599f62ce589fdf1f052513de7
arcobaleno <jymark.borja@gmail.com>
2026-05-07 15:11:59 +0800

    add - auth route group layout with RequireGuest

--- apps/web/pages/(auth)/reset-password/+Page.tsx
+++ apps/web/pages/(auth)/reset-password/+Page.tsx
@@ -1,14 +1,11 @@
 import { ResetPasswordForm } from "@/features/auth/components/ResetPasswordForm";
 import { ResetPasswordSidePanel } from "@/features/auth/components/ResetPasswordSidePanel";
-import { RequireGuest } from "@/features/auth/guards";
 import { AuthShell } from "@/layouts/AuthShell";
 
 export default function Page() {
   return (
-    <RequireGuest>
-      <AuthShell side={<ResetPasswordSidePanel />}>
-        <ResetPasswordForm />
-      </AuthShell>
-    </RequireGuest>
+    <AuthShell side={<ResetPasswordSidePanel />}>
+      <ResetPasswordForm />
+    </AuthShell>
   );
 }
